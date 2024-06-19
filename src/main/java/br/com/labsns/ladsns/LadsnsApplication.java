b3c9125dbd29afe74d11f72685e4d7f93a96695b
Josimar Nepomuceno <josimarjn.dev@gmail.com>
2024-06-19 10:32:47 -0300

    commit inicial

---
+++ src/main/java/br/com/labsns/ladsns/LadsnsApplication.java
@@ -0,0 +1,13 @@
+package br.com.labsns.ladsns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LadsnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LadsnsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/labsns/ladsns/LadsnsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.labsns.ladsns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LadsnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
